b9ac1ecdd3e3a4024e9131e3d03b170b9b32e19b
Albert Attard <albert@javacreed.com>
2015-04-09 13:17:37 +0200

    Support spaces in path

--- virgo-maven-plugin/src/main/java/net/flybyte/virgo/maven/deployer/Deploy.java
+++ virgo-maven-plugin/src/main/java/net/flybyte/virgo/maven/deployer/Deploy.java
@@ -22,29 +22,33 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class Deploy extends BaseMojo {
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		try {
-			logger.info("Start deploying the artefact to Eclipse Virgo");
-			MBeanServerConnection connection = getConnection();
-			// get the Deployer MBean and set up the arguments
-			String artefactPath = getArtefactFile().getAbsolutePath().replaceAll("\\\\", "/");
-			logger.info("Add " + artefactPath + " to the argument list");
-			ObjectName name = new ObjectName(BaseMojo.MBEAN_DEPLOYER);
-			Object[] params  = { "file:///" + artefactPath, isRecoverable() };
-			String[] signature = { "java.lang.String", "boolean" };
-			logger.info("Deploy artifact");
-			// invoke the deploy method of the Deployer MBean
-			connection.invoke(name, "deploy", params, signature);
-		}  catch (Exception e) {
-			throw new MojoFailureException(stackTrace2String(e));
-		} finally {
-			try {
-				closeConnector();
-			} catch (IOException e) {
-				throw new MojoFailureException(stackTrace2String(e));
-			}
-		}
-	}
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            logger.info("Start deploying the artefact to Eclipse Virgo");
+            MBeanServerConnection connection = getConnection();
+            // get the Deployer MBean and set up the arguments
+            String artefactPath = getArtefactFile().getAbsolutePath().replaceAll("\\\\", "/");
+            logger.info("Add " + artefactPath + " to the argument list");
+
+            // Replace the spaces with %20
+            artefactPath = artefactPath.replace(" ", "%20");
+
+            ObjectName name = new ObjectName(BaseMojo.MBEAN_DEPLOYER);
+            Object[] params = { "file:///" + artefactPath, isRecoverable() };
+            String[] signature = { "java.lang.String", "boolean" };
+            logger.info("Deploy artifact");
+            // invoke the deploy method of the Deployer MBean
+            connection.invoke(name, "deploy", params, signature);
+        } catch (Exception e) {
+            throw new MojoFailureException(stackTrace2String(e));
+        } finally {
+            try {
+                closeConnector();
+            } catch (IOException e) {
+                throw new MojoFailureException(stackTrace2String(e));
+            }
+        }
+    }
 
 }
